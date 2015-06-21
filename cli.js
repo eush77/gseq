f678c2ee367d4fe735be64044cad514e7cf6eaa1
Eugene Sharygin <eush77@gmail.com>
2015-06-21 15:47:25 +0300

    Remove undocumented default parameters

--- cli.js
+++ cli.js
@@ -15,26 +15,13 @@ function usage() {
 
 
 (function main(argv) {
-  var first, ratio, last;
-
-  switch (argv.length) {
-    case 1:
-      argv.unshift(1);
-
-    case 2:
-      argv.splice(1, 0, 2);
-
-    case 3:
-      argv = argv.map(Number);
-      first = argv[0];
-      ratio = argv[1];
-      last = argv[2];
-      break;
-
-    default:
-      console.error('Usage:  gseq [<first>] [<ratio>] <end>');
-      process.exit(1);
+  if (argv.length != 3) {
+    return help(1);
   }
+
+  var first = +argv[0],
+      ratio = +argv[1],
+      last = +argv[2];
 
   var print = function (x) { console.log(x) };
 
